105cfad016615fbb8ad83dd2f3925a7660081af7
WGS-94 <wilson-senx@hotmail.com>
2023-06-11 23:51:27 -0300

    :fire: Upload xlsx file, dispaly data and pass by parameter to client

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ from decouple import config
 from matplotlib import pyplot as plt
 from math import pi
 from io import BytesIO
+from windrose import WindroseAxes
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import base64
 import time
+matplotlib.use('agg')
 
 app = Flask(__name__)
 
@@ -22,83 +25,91 @@ def home():
   return render_template('home.html')
 
 @app.route('/generate', methods=['POST'])
-def generate(figure):
+def generate():
   # Your Python script code here
-  buffer = BytesIO()
-  plt.savefig(buffer, format='png')
-  buffer.seek(0)
-  imagem_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
-  return imagem_base64
+  file = request.files['file']
+  if file.filename.endswith('.xlsx'):
+    df = pd.read_excel(file)
+    # Realize aqui a formatação dos dados conforme necessário
+    # print(df)
 
-file = request.files['file']
-if file.filename.endswith('.xlsx'):
-  df = pd.read_excel(file)
-  # Realize aqui a formatação dos dados conforme necessário
-  print(df)
+    # Imagem 01
+    df['velocidad_x'] = df['VelVento'] * np.sin(df['DirVento(°)'] * np.pi / 180.0)
+    df['velocidad_y'] = df['VelVento'] * np.cos(df['DirVento(°)'] * np.pi / 180.0)
+    fig, ax = plt.subplots(figsize=(8, 8), dpi=80)
+    x0, x1 = ax.get_xlim()
+    y0, y1 = ax.get_ylim()
+    ax.set_aspect('equal')
+    _ = df.plot(kind='scatter', x='velocidad_x', y='velocidad_y', alpha=0.35, ax=ax)
+    imagem_01 = 'upload/windrose_01.png'
+    plt.savefig(imagem_01)
 
-  # Imagem 01
-  df['velocidad_x'] = df['VelVento'] * np.sin(df['DirVento(°)'] * np.pi / 180.0)
-  df['velocidad_y'] = df['VelVento'] * np.cos(df['DirVento(°)'] * np.pi / 180.0)
-  fig, ax = plt.subplots(figsize=(8, 8), dpi=80)
-  x0, x1 = ax.get_xlim()
-  y0, y1 = ax.get_ylim()
-  ax.set_aspect('equal')
-  _ = df.plot(kind='scatter', x='velocidad_x', y='velocidad_y', alpha=0.35, ax=ax)
-  imagem_01 = generate(fig)
+    # Imagem 02
+    fig, ax = plt.subplots()
+    ax = WindroseAxes.from_ax()
+    ax.bar(df['DirVento(°)'], df['VelVento'], normed=True, opening=0.8, edgecolor='white')
+    ax.set_legend()
+    imagem_02 = 'upload/windrose_02.png'  
+    plt.savefig(imagem_02)
 
-  # Imagem 02
-  fig, ax = plt.subplots()
-  ax = WindroseAxes.from_ax()
-  ax.bar(df['DirVento(°)'], df['VelVento'], normed=True, opening=0.8, edgecolor='white')
-  ax.set_legend()
-  imagem_02 = generate(fig)
+    imagens = [imagem_01, imagem_02]
 
-  # Imagem 03
-  fig, ax = plt.subplots()
-  ax = WindroseAxes.from_ax()
-  ax.bar(df['DirVento(°)'], df['VelVento'], normed=True, opening=0.8, edgecolor='white', cmap=cm.hot)
-  ax.set_legend()
-  imagem_03 = generate(fig)
+    # host = 'http://127.0.0.1:5000'
 
-  # Imagem 04
-  fig, ax = plt.subplots()
-  ax = WindroseAxes.from_ax()
-  ax.box(df['DirVento(°)'], df['VelVento'], bins=np.arange(0, 8, 1))
-  ax.set_legend()
-  imagem_04 = generate(fig)
+    # urls_imagens = [f"{host}/{imagem}" for imagem in imagens]
 
-  # Imagem 05
-  fig, ax = plt.subplots()
-  ax = WindroseAxes.from_ax()
-  ax.contourf(df['DirVento(°)'], df['VelVento'], bins=np.arange(0, 8, 1), cmap=cm.hot)
-  ax.set_legend()
-  imagem_05 = generate(fig)
+    print(imagens)
 
-  # Imagem 06
-  fig, ax = plt.subplots()
-  ax = WindroseAxes.from_ax()
-  ax.contourf(df['DirVento(°)'], df['VelVento'], bins=np.arange(0, 8, 1), cmap=cm.hot)
-  ax.contour(df['DirVento(°)'], df['VelVento'], bins=np.arange(0, 8, 1), colors='black')
-  ax.set_legend()
-  imagem_06 = generate(fig)
+    # # Imagem 03
+    # figure, ax = plt.subplots()
+    # ax = WindroseAxes.from_ax()
+    # ax.bar(df['DirVento(°)'], df['VelVento'], normed=True, opening=0.8, edgecolor='white', cmap=cm.hot)
+    # ax.set_legend()
+    # imagem_03 = 'windrose_03.png'
 
-  # Imagem 07
-  fig, ax = plt.subplots()
-  ax = WindroseAxes.from_ax()
-  ax.contour(df['DirVento(°)'], df['VelVento'], bins=np.arange(0, 8, 1), cmap=cm.hot, lw=3)
-  ax.set_legend()
-  imagem_07 = generate(fig)
+    # # Imagem 04
+    # figure, ax = plt.subplots()
+    # ax = WindroseAxes.from_ax()
+    # ax.box(df['DirVento(°)'], df['VelVento'], bins=np.arange(0, 8, 1))
+    # ax.set_legend()
+    # imagem_04 = 'windrose_04.png'
 
-  # Salvar as imagens em um array
-  imagens = [imagem_01, imagem_02, imagem_03, imagem_04, imagem_05, imagem_06, imagem_07]
+    # # Imagem 05
+    # figure, ax = plt.subplots()
+    # ax = WindroseAxes.from_ax()
+    # ax.contourf(df['DirVento(°)'], df['VelVento'], bins=np.arange(0, 8, 1), cmap=cm.hot)
+    # ax.set_legend()
+    # imagem_05 = 'windrose_05.png'
 
-  # Exibir o array de imagens no cliente da aplicação
+    # # Imagem 06
+    # figure, ax = plt.subplots()
+    # ax = WindroseAxes.from_ax()
+    # ax.contourf(df['DirVento(°)'], df['VelVento'], bins=np.arange(0, 8, 1), cmap=cm.hot)
+    # ax.contour(df['DirVento(°)'], df['VelVento'], bins=np.arange(0, 8, 1), colors='black')
+    # ax.set_legend()
+    # imagem_06 = 'windrose_06.png'
 
-  flash("Upload e formatação concluídos com sucesso!")
-  return render_template('success.html', imagens=imagens)
-else:
-  return 'Por favor, faça o upload de um arquivo XLSX.'
-# flash("Data Inserted Successfully")
+    # # Imagem 07
+    # figure, ax = plt.subplots()
+    # ax = WindroseAxes.from_ax()
+    # ax.contour(df['DirVento(°)'], df['VelVento'], bins=np.arange(0, 8, 1), cmap=cm.hot, lw=3)
+    # ax.set_legend()
+    # imagem_07 = 'windrose_07.png'
+
+    # # Salvar as imagens em um array
+    # imagens = [imagem_01, imagem_02, imagem_03, imagem_04, imagem_05, imagem_06, imagem_07]
+
+    flash("Upload e formatação concluídos com sucesso!")
+
+    # Exibir o array de imagens no cliente da aplicação
+    return render_template('success.html', imagens=imagens)
+  else:
+    return 'Por favor, faça o upload de um arquivo XLSX.'
+
+# Exibir o array de imagens no cliente da aplicação
+# @app.route('/sucess')
+# def sucess():
+#     return render_template('sucess.html', imagens=imagens)
 # time.sleep(5)
 
 if __name__ == '__main__':
