88e21eb4cda0208a72faa55309ebf6cbf1132209
WGS-94 <wilson-senx@hotmail.com>
2023-06-11 21:00:16 -0300

    :tada: Upload xlsx file and dispaly data using pandas

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, redirect, request, session, jsonify, flash, url_for
 from decouple import config
 import pandas as pd
+import matplotlib.pyplot as plt
 import time
 
 app = Flask(__name__)
@@ -20,8 +21,18 @@ def generate():
   if file.filename.endswith('.xlsx'):
     df = pd.read_excel(file)
     # Realize aqui a formatação dos dados conforme necessário
-    # Por exemplo, você pode imprimir as colunas e os valores:
     print(df)
+
+    # Geramos um gráfico de pontos de velocidade x vs. velocidade y
+    df['velocidad_x'] = df['VelVento'] * np.sin(df['DirVento(°)'] * pi / 180.0)
+    df['velocidad_y'] = df['VelVento'] * np.cos(df['DirVento(°)'] * pi / 180.0)
+    fig, ax = plt.subplots(figsize=(8, 8), dpi=80)
+    x0, x1 = ax.get_xlim()
+    y0, y1 = ax.get_ylim()
+    ax.set_aspect('equal')
+    _ = df.plot(kind='scatter', x='velocidad_x', y='velocidad_y', alpha=0.35, ax=ax)
+
+    # Por exemplo, você pode imprimir as colunas e os valores:
     # print(df.columns)
     # print(df.values)
     flash("Upload e formatação concluídos com sucesso!")
